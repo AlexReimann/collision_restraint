b3c63fd249d6b094ab9281a3bfba836c20be57b1
Alex <reimann.alex88@gmail.com>
2025-03-09 08:22:06 +0900

    Use format for source prefix

--- include/collision_restraint/utility.hpp
+++ include/collision_restraint/utility.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <format>
 #include <source_location>
-#include <stdexcept>
 #include <string>
 
 namespace collision_restraint
@@ -10,8 +10,9 @@ namespace collision_restraint
 [[maybe_unused]] static std::string source_prefix(
   std::source_location location = std::source_location::current())
 {
-  return std::string(location.file_name()) + ":" + std::to_string(location.line()) + ":" +
-         std::to_string(location.column()) + ":" + location.function_name() + ": ";
+  return std::format(
+    "{}:{}:{}:{}", location.file_name(), location.line(), location.column(),
+    location.function_name());
 }
 
 }  // namespace collision_restraint
