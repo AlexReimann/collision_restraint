61cfb8eb9e325f542d16502e54a15346b482a781
Alex <reimann.alex88@gmail.com>
2025-03-09 09:14:03 +0900

    Implement distance calc in Model, except angular

--- include/collision_restraint/model.hpp
+++ include/collision_restraint/model.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "collision_restraint/footprint.hpp"
+#include "collision_restraint/polar_point.hpp"
 
 namespace collision_restraint
 {
@@ -12,13 +13,31 @@ public:
 
   void setVelocities(const float linear, const float angular);
 
-  [[nodiscard]] float get(const float x, const float y) const;
+  [[nodiscard]] bool isStraight() const;
+  [[nodiscard]] bool isLeftTurn() const;
+
+  [[nodiscard]] float innerRadius() const;
+  [[nodiscard]] float centerRadius() const;
+  [[nodiscard]] float outerRadius() const;
+
+  [[nodiscard]] float distance(const float x, const float y) const;
 
 private:
+  [[nodiscard]] bool insideFootprint(const float x, const float y) const;
+  [[nodiscard]] float straightDistance(const float x, const float y) const;
+  [[nodiscard]] float angularDistance(const PolarPoint & point) const;
+
   Footprint footprint_;
 
   float velocity_linear_;
   float velocity_angular_;
+
+  bool straight_;
+  bool left_turn_;
+
+  float inner_radius_;
+  float center_radius_;
+  float outer_radius_;
 };
 
 }  // namespace collision_restraint
--- src/model.cpp
+++ src/model.cpp
@@ -1,8 +1,6 @@
 #include "collision_restraint/model.hpp"
 
 #include <cmath>
-#include <format>
-#include <stdexcept>
 
 #include "collision_restraint/polar_point.hpp"
 #include "collision_restraint/utility.hpp"
@@ -16,13 +14,89 @@ void Model::setVelocities(const float linear, const float angular)
 {
   velocity_linear_ = linear;
   velocity_angular_ = angular;
+
+  constexpr float straight_threshold = 0.001F;
+  straight_ = std::abs(velocity_angular_) <= straight_threshold;
+  left_turn_ = velocity_angular_ >= 0.0F;
+
+  if (straight_) {
+    inner_radius_ = std::numeric_limits<float>::infinity();
+    center_radius_ = std::numeric_limits<float>::infinity();
+    outer_radius_ = std::numeric_limits<float>::infinity();
+    return;
+  }
+
+  center_radius_ = velocity_linear_ / velocity_angular_;
+  inner_radius_ = center_radius_ - footprint_.halfWidth();
+
+  const float max_offset =
+    std::max(std::abs(footprint_.offsetFront()), std::abs(footprint_.offsetBack()));
+  const float radius_perpendicular = center_radius_ + footprint_.halfWidth();
+  outer_radius_ =
+    std::sqrt((max_offset * max_offset) + (radius_perpendicular * radius_perpendicular));
 }
 
-float Model::get(const float x, const float y) const
+bool Model::isStraight() const { return straight_; }
+bool Model::isLeftTurn() const { return left_turn_; }
+
+float Model::innerRadius() const { return inner_radius_; }
+float Model::centerRadius() const { return center_radius_; }
+float Model::outerRadius() const { return outer_radius_; }
+
+float Model::distance(const float x, const float y) const
 {
   PolarPoint point{x, y};
 
+  if (insideFootprint(point.x(), point.y())) {
+    return 0.0F;
+  }
+
+  if (velocity_linear_ == 0.0F) {
+    return std::numeric_limits<float>::infinity();
+  }
+
+  if (straight_) {
+    return straightDistance(point.x(), point.y());
+  }
+
+  return angularDistance(point);
+}
+
+bool Model::insideFootprint(const float x, const float y) const
+{
+  if (std::abs(y) > footprint_.halfWidth()) {
+    return false;
+  }
+
+  if (x > footprint_.offsetFront() || x < footprint_.offsetBack()) {
+    return false;
+  }
+
+  return true;
+}
+
+float Model::straightDistance(const float x, const float y) const
+{
+  if (std::abs(y) > footprint_.halfWidth()) {
+    return std::numeric_limits<float>::infinity();
+  }
+
+  if (velocity_linear_ < 0.0F && x < footprint_.offsetBack()) {
+    return std::abs(x - footprint_.offsetBack());
+  }
+
+  if (velocity_linear_ > 0.0F && x > footprint_.offsetFront()) {
+    return std::abs(x - footprint_.offsetFront());
+  }
+
   return 0.0F;
-};
+}
+
+float Model::angularDistance(const PolarPoint & point) const
+{
+  // Use line in polar coordinates function to calculate angular point distance
+  // Check if actually inside circles
+  return point.theta();
+}
 
 }  // namespace collision_restraint
