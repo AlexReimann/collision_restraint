e7c34c2e5ec169fde3ed91c6c17b0a27d19ad019
Alex <reimann.alex88@gmail.com>
2025-03-21 20:48:10 +0900

    Add Params::ignore_inside_footprint_

--- include/collision_restraint/params.hpp
+++ include/collision_restraint/params.hpp
@@ -7,11 +7,12 @@ class Params
 public:
   Params(
     const float deceleration, const float execution_delay, const float min_obstacle_height,
-    const float max_obstacle_height)
+    const float max_obstacle_height, const bool ignore_inside_footprint)
   : deceleration_{deceleration},
     execution_delay_{execution_delay},
     min_obstacle_height_{min_obstacle_height},
-    max_obstacle_height_{max_obstacle_height}
+    max_obstacle_height_{max_obstacle_height},
+    ignore_inside_footprint_{ignore_inside_footprint}
   {
   }
 
@@ -21,12 +22,16 @@ public:
   [[nodiscard]] float minObstacleHeight() const { return min_obstacle_height_; }
   [[nodiscard]] float maxObstacleHeight() const { return max_obstacle_height_; }
 
+  [[nodiscard]] bool ignoreInsideFootrpint() const { return ignore_inside_footprint_; }
+
 private:
   float deceleration_;
   float execution_delay_;
 
   float min_obstacle_height_;
   float max_obstacle_height_;
+
+  bool ignore_inside_footprint_;
 };
 
 }  // namespace collision_restraint
