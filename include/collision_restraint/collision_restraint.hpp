c7d3af828b233374d879205b9c1ef6c6141cec46
Alex <reimann.alex88@gmail.com>
2025-03-21 20:43:26 +0900

    Implement constructor, restrain() of CollisionRestraint

--- include/collision_restraint/collision_restraint.hpp
+++ include/collision_restraint/collision_restraint.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tuple>
+
+#include "collision_restraint/distance_model.hpp"
+#include "collision_restraint/footprint.hpp"
+#include "collision_restraint/motion_model.hpp"
+#include "collision_restraint/params.hpp"
+#include "collision_restraint/velocities.hpp"
 #include "collision_restraint/visibility_control.h"
 
 namespace collision_restraint
@@ -8,7 +16,16 @@ namespace collision_restraint
 class CollisionRestraint
 {
 public:
-  CollisionRestraint();
+  CollisionRestraint(Footprint footprint, const Params & params);
+
+  [[nodiscard]] std::tuple<bool, Velocities, PolarPoint> restrain(
+    const Velocities & velocities, const sensor_msgs::msg::PointCloud2 & point_cloud) const;
+
+private:
+  Params params_;
+
+  DistanceModel distance_;
+  MotionModel motion_;
 };
 
 }  // namespace collision_restraint
--- src/collision_restraint.cpp
+++ src/collision_restraint.cpp
@@ -1,10 +1,50 @@
 #include "collision_restraint/collision_restraint.hpp"
 
+#include <limits>
+#include <sensor_msgs/point_cloud2_iterator.hpp>
+
 namespace collision_restraint
 {
 
-CollisionRestraint::CollisionRestraint()
+CollisionRestraint::CollisionRestraint(Footprint footprint, const Params & params)
+: params_{params}, distance_{footprint}, motion_{params.deceleration(), params.executionDelay()}
 {
+}
+
+std::tuple<bool, Velocities, PolarPoint> CollisionRestraint::restrain(
+  const Velocities & velocities, const sensor_msgs::msg::PointCloud2 & point_cloud) const
+{
+  sensor_msgs::PointCloud2ConstIterator<float> iter_x(point_cloud, "x");
+  sensor_msgs::PointCloud2ConstIterator<float> iter_y(point_cloud, "y");
+  sensor_msgs::PointCloud2ConstIterator<float> iter_z(point_cloud, "z");
+
+  float min_arc_distance = std::numeric_limits<float>::infinity();
+  float x = 0.0F;
+  float y = 0.0F;
+
+  for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z) {
+    if (*iter_z < params_.minObstacleHeight() || *iter_z > params_.maxObstacleHeight()) {
+      continue;
+    }
+
+    const float arc_distance = distance_.arcDistance(*iter_x, *iter_y);
+
+    if (arc_distance < min_arc_distance) {
+      min_arc_distance = arc_distance;
+      x = *iter_x;
+      y = *iter_y;
+    }
+  }
+
+  const PolarPoint closest_point{x, y};
+  const float min_distance = PolarPoint::arcToEucDistance(min_arc_distance, closest_point.r());
+  const float stop_distance = motion_.stoppingDistance(velocities.linear_);
+
+  if (min_distance > stop_distance) {
+    return {false, velocities, closest_point};
+  }
+
+  return {true, motion_.scaleToStopDistance(velocities, min_distance), closest_point};
 }
 
 }  // namespace collision_restraint
