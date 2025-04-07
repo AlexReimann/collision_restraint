50f3f39850618d1e2e96dcf1dd8459d7334aec51
Alex <reimann.alex88@gmail.com>
2025-04-07 21:16:45 +0900

    Fix outer radius visualization offset

--- src/visualization.cpp
+++ src/visualization.cpp
@@ -39,7 +39,7 @@ visualization_msgs::msg::MarkerArray Visualization::trajectoryMarker(
 
   const bool left = angular_velocity >= 0.0 ? true : false;
 
-  visualization_msgs::msg::Marker outer = circleMarker(radii.outer_, -footprint_.halfWidth(), left);
+  visualization_msgs::msg::Marker outer = circleMarker(radii.outer_, -(radii.outer_ - radii.center_), left);
   array.markers.push_back(outer);
 
   if (std::isnormal(radii.center_)) {
